f3ba2618f9a4ddb5b5a31364dfd76be1fc08e044
Mojalefa <cmtsoaeli@gmail.com>
2019-09-25 01:49:50 +0200

    latest

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Button as BaseButton } from 'antd'
 import './Button.scss';
-import classNames from 'classnames'
+import classNames from 'classnames';
 
 const Button = props => (
      <BaseButton 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ import './Home.scss'
 const Home = () => (
     <section className="home">
         <Message 
-            avatar="https://pbs.twimg.com/profile_images/1102277767524163584/7lJaR7qP_200x200.jpg"
+            avatar="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
             text="hello" 
             date="Mon Apr 22 2019 21:29"
         />
